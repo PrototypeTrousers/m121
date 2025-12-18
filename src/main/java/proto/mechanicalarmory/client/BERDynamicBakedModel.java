67663eef6bd007977a125cd7d93230ea6cd60515
PrototypeTrousers <salomaoo@gmail.com>
2025-12-18 12:35:05 -0300

    renders a cube

---
+++ src/main/java/proto/mechanicalarmory/client/BERDynamicBakedModel.java
@@ -0,0 +1,57 @@
+package proto.mechanicalarmory.client;
+
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.block.model.BakedQuad;
+import net.minecraft.client.renderer.block.model.ItemOverrides;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.core.Direction;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.block.state.BlockState;
+import net.neoforged.neoforge.client.model.IDynamicBakedModel;
+import net.neoforged.neoforge.client.model.data.ModelData;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class BERDynamicBakedModel implements IDynamicBakedModel {
+    List<BakedQuad> quads;
+
+    public BERDynamicBakedModel(List<BakedQuad> quads) {
+        this.quads = quads;
+    }
+
+    @Override
+    public List<BakedQuad> getQuads(@Nullable BlockState state, @Nullable Direction side, RandomSource rand, ModelData extraData, @Nullable RenderType renderType) {
+        return quads;
+    }
+
+    @Override
+    public boolean useAmbientOcclusion() {
+        return false;
+    }
+
+    @Override
+    public boolean isGui3d() {
+        return false;
+    }
+
+    @Override
+    public boolean usesBlockLight() {
+        return false;
+    }
+
+    @Override
+    public boolean isCustomRenderer() {
+        return false;
+    }
+
+    @Override
+    public TextureAtlasSprite getParticleIcon() {
+        return null;
+    }
+
+    @Override
+    public ItemOverrides getOverrides() {
+        return null;
+    }
+}
--- src/main/java/proto/mechanicalarmory/client/instances/ArmVisual.java
+++ src/main/java/proto/mechanicalarmory/client/instances/ArmVisual.java
@@ -1,8 +1,11 @@
 package proto.mechanicalarmory.client.instances;
 
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.*;
 import dev.engine_room.flywheel.api.instance.Instance;
 import dev.engine_room.flywheel.api.material.CardinalLightingMode;
 import dev.engine_room.flywheel.api.material.Material;
+import dev.engine_room.flywheel.api.model.Model;
 import dev.engine_room.flywheel.api.task.Plan;
 import dev.engine_room.flywheel.api.visual.DynamicVisual;
 import dev.engine_room.flywheel.api.visual.LightUpdatedVisual;
@@ -12,33 +15,62 @@ import dev.engine_room.flywheel.lib.instance.TransformedInstance;
 import dev.engine_room.flywheel.lib.material.SimpleMaterial;
 import dev.engine_room.flywheel.lib.model.ModelUtil;
 import dev.engine_room.flywheel.lib.model.Models;
+import dev.engine_room.flywheel.lib.model.baked.BakedModelBuilder;
 import dev.engine_room.flywheel.lib.model.baked.BlockModelBuilder;
+import dev.engine_room.flywheel.lib.model.baked.PartialModel;
 import dev.engine_room.flywheel.lib.model.baked.SinglePosVirtualBlockGetter;
 import dev.engine_room.flywheel.lib.model.part.InstanceTree;
 import dev.engine_room.flywheel.lib.model.part.ModelTree;
+import dev.engine_room.flywheel.lib.model.part.ModelTrees;
 import dev.engine_room.flywheel.lib.task.RunnablePlan;
 import dev.engine_room.flywheel.lib.util.RecyclingPoseStack;
 import dev.engine_room.flywheel.lib.visual.AbstractBlockEntityVisual;
 import dev.engine_room.vanillin.item.ItemModels;
 import it.unimi.dsi.fastutil.longs.LongSet;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.model.geom.ModelLayers;
 import net.minecraft.client.renderer.LevelRenderer;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderBuffers;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.block.BlockRenderDispatcher;
+import net.minecraft.client.renderer.block.model.BakedQuad;
+import net.minecraft.client.renderer.blockentity.BellRenderer;
+import net.minecraft.client.renderer.texture.TextureAtlas;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.client.resources.model.UnbakedModel;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.core.SectionPos;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.inventory.InventoryMenu;
 import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.MilkBucketItem;
 import net.minecraft.world.level.BlockAndTintGetter;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.entity.BellBlockEntity;
+import net.minecraft.world.level.block.entity.ChestBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.api.distmarker.OnlyIn;
+import net.neoforged.neoforge.client.extensions.common.IClientItemExtensions;
+import net.neoforged.neoforge.client.model.pipeline.QuadBakingVertexConsumer;
+import net.neoforged.neoforge.client.model.pipeline.RemappingVertexPipeline;
 import org.jetbrains.annotations.Nullable;
 import org.joml.Matrix4f;
 import org.joml.Matrix4fc;
+import org.joml.Vector2f;
+import org.joml.Vector3f;
 import proto.mechanicalarmory.MechanicalArmoryClient;
+import proto.mechanicalarmory.client.BERDynamicBakedModel;
+import proto.mechanicalarmory.client.mixin.BufferSourceAccessor;
 import proto.mechanicalarmory.common.entities.block.ArmEntity;
 
-import java.util.ArrayList;
-import java.util.Collections;
+import java.nio.ByteBuffer;
+import java.nio.IntBuffer;
+import java.util.*;
 import java.util.function.Consumer;
 
 public class ArmVisual extends AbstractBlockEntityVisual<ArmEntity> implements DynamicVisual, LightUpdatedVisual {
@@ -49,6 +81,10 @@ public class ArmVisual extends AbstractBlockEntityVisual<ArmEntity> implements D
     private final @Nullable InstanceTree secondArm;
     private final @Nullable InstanceTree baseMotor;
     private final TransformedInstance cactus;
+
+    private static final Material MATERIAL = SimpleMaterial.builder()
+            .mipmap(false)
+            .build();
 
     private final RecyclingPoseStack poseStack = new RecyclingPoseStack();
     private final Matrix4fc initialPose;
@@ -62,27 +98,104 @@ public class ArmVisual extends AbstractBlockEntityVisual<ArmEntity> implements D
         firstArm = baseMotor.child("FirstArm");
         secondArm = firstArm.child("SecondArm");
 
+        var br = new RenderBuffers(1);
+
+
         SinglePosVirtualBlockGetter lv = SinglePosVirtualBlockGetter.createFullBright();
-        lv.blockState(Blocks.BELL.defaultBlockState());
-        lv.blockEntity(new BellBlockEntity(pos, Blocks.BELL.defaultBlockState()));
+        lv.blockState(Blocks.CHEST.defaultBlockState());
+        ChestBlockEntity bbe = new ChestBlockEntity(pos, Blocks.CHEST.defaultBlockState());
+        lv.blockEntity(bbe);
         lv.pos(pos);
+        PoseStack pose = new PoseStack();
+        pose.pushPose();
+        pose.setIdentity();
+        //pose.translate(pos.getX(), pos.getY(), pos.getZ());
+        Minecraft.getInstance().getBlockRenderer().renderSingleBlock(Blocks.DIRT.defaultBlockState(), pose, br.bufferSource(), computePackedLight(), 0);
+        //Minecraft.getInstance().getBlockEntityRenderDispatcher().renderItem(bbe, new PoseStack(), br.bufferSource(), computePackedLight(), 0);
 
-        var model = new BlockModelBuilder(lv, Collections.singleton(pos))
-                .materialFunc((renderType, shaded, ao) -> {
-                    Material material = ModelUtil.getMaterial(renderType, shaded, ao);
-                    if (material != null && material.cardinalLightingMode() == CardinalLightingMode.ENTITY) {
-                        return SimpleMaterial.builderOf(material)
-                                .cardinalLightingMode(CardinalLightingMode.CHUNK)
-                                .build();
-                    } else {
-                        return material;
-                    }
-                })
-                .build();
+        MultiBufferSource.BufferSource bs = br.bufferSource();
+        Map<RenderType, BufferBuilder> mp = ((BufferSourceAccessor) bs).getStartedBuilders();
 
+        List<BakedQuad> bakedQuadList = new ArrayList<>();
+
+        for (Map.Entry<RenderType, BufferBuilder> entry : mp.entrySet()) {
+            MeshData meshData = entry.getValue().build();
+            if (meshData == null) continue;
+
+            VertexFormat format = meshData.drawState().format();
+            int vertexSizeInBytes = format.getVertexSize();
+            int vertexSizeInInts = vertexSizeInBytes / 4;
+
+            ByteBuffer buffer = meshData.vertexBuffer();
+            while (buffer.hasRemaining()) {
+                int[] aint = new int[32];
+                for (int i = 0; i < 4; i++) {
+                    float x = buffer.getFloat();
+                    float y = buffer.getFloat();
+                    float z = buffer.getFloat();
+
+                    int color = buffer.getInt();
+
+                    float u = buffer.getFloat();
+                    float v = buffer.getFloat();
+
+                    int packedOverlay = buffer.getShort();
+                    int packedLight = buffer.getShort();
+
+                    int normal = buffer.getInt();
+
+                    int xInt = Float.floatToRawIntBits(x);
+                    int yInt = Float.floatToRawIntBits(y);
+                    int zInt = Float.floatToRawIntBits(z);
+
+                    int intu = Float.floatToRawIntBits(u);
+                    int intv = Float.floatToRawIntBits(v);
+
+                    int destIndex = i * 7;
+
+                    aint[i * 7] = xInt;
+                    aint[i * 7 + 1] = yInt;
+                    aint[i * 7 + 2] = zInt;
+                    aint[i * 7 + 3] = color;
+                    aint[i * 7 + 4] = intu;
+                    aint[i * 7 + 5] = intv;
+                    aint[i * 7 + 6] = packedOverlay;
+                    aint[i * 7 + 7] = packedLight;
+                    aint[i * 7 + 8] = normal;
+                }
+                buffer.getInt();
+                buffer.getInt();
+                buffer.getInt();
+                buffer.getInt();
+
+                bakedQuadList.add(new BakedQuad(aint.clone(), -1, Direction.UP, Minecraft.getInstance().getTextureAtlas(TextureAtlas.LOCATION_BLOCKS).apply(ResourceLocation.withDefaultNamespace("dirt")), false));
+            }
+        }
+
+//        vertexData[i] = Float.floatToRawIntBits(vector.x());
+//        vertexData[i + 1] = Float.floatToRawIntBits(vector.y());
+//        vertexData[i + 2] = Float.floatToRawIntBits(vector.z());
+//        vertexData[i + 3] = -1;
+//        vertexData[i + 4] = Float.floatToRawIntBits(sprite.getU(blockFaceUV.getU(vertexIndex) / 16.0F));
+//        vertexData[i + 4 + 1] = Float.floatToRawIntBits(sprite.getV(blockFaceUV.getV(vertexIndex) / 16.0F));
+
+//        float x,
+//        float y,
+//        float z,
+//        int color,
+//        float u,
+//        float v,
+//        int packedOverlay,
+//        int packedLight,
+//        float normalX,
+//        float normalY,
+//        float normalZ
+
+        BERDynamicBakedModel berDynamicBakedModel = new BERDynamicBakedModel(bakedQuadList);
+        BakedModelBuilder bakedModelBuilder = new BakedModelBuilder(berDynamicBakedModel);
 
         cactus = instancerProvider()
-                .instancer(InstanceTypes.TRANSFORMED, model)
+                .instancer(InstanceTypes.TRANSFORMED, bakedModelBuilder.build())
                 .createInstance();
     }
 
