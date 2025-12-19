8675393c01d4121bc1df4e73556824698f75508e
PrototypeTrousers <salomaoo@gmail.com>
2025-12-18 21:21:20 -0300

    display BER + Block

--- src/main/java/proto/mechanicalarmory/client/instances/ArmVisual.java
+++ src/main/java/proto/mechanicalarmory/client/instances/ArmVisual.java
@@ -1,9 +1,8 @@
 package proto.mechanicalarmory.client.instances;
 
-import com.mojang.blaze3d.systems.RenderSystem;
-import com.mojang.blaze3d.vertex.*;
+import com.mojang.blaze3d.vertex.BufferBuilder;
+import com.mojang.blaze3d.vertex.PoseStack;
 import dev.engine_room.flywheel.api.instance.Instance;
-import dev.engine_room.flywheel.api.material.CardinalLightingMode;
 import dev.engine_room.flywheel.api.material.Material;
 import dev.engine_room.flywheel.api.task.Plan;
 import dev.engine_room.flywheel.api.visual.DynamicVisual;
@@ -12,73 +11,32 @@ import dev.engine_room.flywheel.api.visualization.VisualizationContext;
 import dev.engine_room.flywheel.lib.instance.InstanceTypes;
 import dev.engine_room.flywheel.lib.instance.TransformedInstance;
 import dev.engine_room.flywheel.lib.material.SimpleMaterial;
-import dev.engine_room.flywheel.lib.model.ModelUtil;
 import dev.engine_room.flywheel.lib.model.Models;
-import dev.engine_room.flywheel.lib.model.SimpleModel;
-import dev.engine_room.flywheel.lib.model.baked.BakedModelBuilder;
-import dev.engine_room.flywheel.lib.model.baked.BlockModelBuilder;
-import dev.engine_room.flywheel.lib.model.baked.PartialModel;
 import dev.engine_room.flywheel.lib.model.baked.SinglePosVirtualBlockGetter;
 import dev.engine_room.flywheel.lib.model.part.InstanceTree;
 import dev.engine_room.flywheel.lib.model.part.ModelTree;
-import dev.engine_room.flywheel.lib.model.part.ModelTrees;
 import dev.engine_room.flywheel.lib.task.RunnablePlan;
 import dev.engine_room.flywheel.lib.util.RecyclingPoseStack;
 import dev.engine_room.flywheel.lib.visual.AbstractBlockEntityVisual;
-import dev.engine_room.vanillin.item.ItemModels;
 import it.unimi.dsi.fastutil.longs.LongSet;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.model.Model;
-import net.minecraft.client.model.geom.ModelLayers;
-import net.minecraft.client.renderer.*;
-import net.minecraft.client.renderer.block.BlockRenderDispatcher;
-import net.minecraft.client.renderer.block.model.BakedQuad;
-import net.minecraft.client.renderer.blockentity.BellRenderer;
-import net.minecraft.client.renderer.texture.TextureAtlas;
-import net.minecraft.client.renderer.texture.TextureAtlasSprite;
-import net.minecraft.client.resources.model.AtlasSet;
-import net.minecraft.client.resources.model.UnbakedModel;
-import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
+import net.minecraft.client.renderer.LevelRenderer;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderBuffers;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.core.SectionPos;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.inventory.InventoryMenu;
-import net.minecraft.world.item.ItemDisplayContext;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
-import net.minecraft.world.item.MilkBucketItem;
-import net.minecraft.world.level.BlockAndTintGetter;
 import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.DirtPathBlock;
-import net.minecraft.world.level.block.entity.BedBlockEntity;
-import net.minecraft.world.level.block.entity.BellBlockEntity;
-import net.minecraft.world.level.block.entity.ChestBlockEntity;
-import net.minecraft.world.level.block.entity.EnderChestBlockEntity;
-import net.minecraft.world.level.block.state.BlockState;
-import net.neoforged.api.distmarker.Dist;
-import net.neoforged.api.distmarker.OnlyIn;
-import net.neoforged.neoforge.client.extensions.common.IClientItemExtensions;
-import net.neoforged.neoforge.client.model.pipeline.QuadBakingVertexConsumer;
-import net.neoforged.neoforge.client.model.pipeline.RemappingVertexPipeline;
+import net.minecraft.world.level.block.entity.EnchantingTableBlockEntity;
 import org.jetbrains.annotations.Nullable;
 import org.joml.Matrix4f;
 import org.joml.Matrix4fc;
-import org.joml.Vector2f;
-import org.joml.Vector3f;
 import proto.mechanicalarmory.MechanicalArmoryClient;
-import proto.mechanicalarmory.client.BERDynamicBakedModel;
 import proto.mechanicalarmory.client.CapturedModel;
 import proto.mechanicalarmory.client.mixin.BufferSourceAccessor;
-import proto.mechanicalarmory.client.mixin.RenderTypeAccessor;
-import proto.mechanicalarmory.client.mixin.TextureStateShardAccessor;
 import proto.mechanicalarmory.common.entities.block.ArmEntity;
 
-import java.nio.ByteBuffer;
-import java.nio.IntBuffer;
-import java.util.*;
+import java.util.Map;
 import java.util.function.Consumer;
-
-import static net.minecraft.client.renderer.Sheets.BED_SHEET;
 
 public class ArmVisual extends AbstractBlockEntityVisual<ArmEntity> implements DynamicVisual, LightUpdatedVisual {
 
@@ -87,7 +45,8 @@ public class ArmVisual extends AbstractBlockEntityVisual<ArmEntity> implements D
     private final @Nullable InstanceTree firstArm;
     private final @Nullable InstanceTree secondArm;
     private final @Nullable InstanceTree baseMotor;
-    private final TransformedInstance cactus;
+    private final TransformedInstance transformedInstance1;
+    private final TransformedInstance transformedInstance2;
 
     private static final Material MATERIAL = SimpleMaterial.builder()
             .mipmap(false)
@@ -106,18 +65,20 @@ public class ArmVisual extends AbstractBlockEntityVisual<ArmEntity> implements D
         secondArm = firstArm.child("SecondArm");
 
         var br = new RenderBuffers(1);
-
-
         SinglePosVirtualBlockGetter lv = SinglePosVirtualBlockGetter.createFullBright();
-        lv.blockState(Blocks.BLACK_BED.defaultBlockState());
-        BedBlockEntity bbe = new BedBlockEntity(pos, Blocks.BLACK_BED.defaultBlockState());
+        lv.blockState(Blocks.ENCHANTING_TABLE.defaultBlockState());
+        EnchantingTableBlockEntity bbe = new EnchantingTableBlockEntity(pos, Blocks.ENCHANTING_TABLE.defaultBlockState());
         lv.blockEntity(bbe);
         lv.pos(pos);
-        PoseStack pose = new PoseStack();
-        pose.pushPose();
-        pose.setIdentity();
+        bbe.setLevel(level);
+
+        transformedInstance2 = instancerProvider()
+                .instancer(InstanceTypes.TRANSFORMED, Models.block(Blocks.ENCHANTING_TABLE.defaultBlockState()))
+                .createInstance();
+
         //pose.translate(pos.getX(), pos.getY(), pos.getZ());
 //        Minecraft.getInstance().getBlockRenderer().renderSingleBlock(Blocks.DIRT.defaultBlockState(), pose, br.bufferSource(), computePackedLight(), 0);
+        //Minecraft.getInstance().getBlockEntityRenderDispatcher().render(bbe, partialTick, new PoseStack(), br.bufferSource());
         Minecraft.getInstance().getBlockEntityRenderDispatcher().renderItem(bbe, new PoseStack(), br.bufferSource(), LevelRenderer.getLightColor(level, pos.above()), 0);
 
         MultiBufferSource.BufferSource bs = br.bufferSource();
@@ -125,7 +86,7 @@ public class ArmVisual extends AbstractBlockEntityVisual<ArmEntity> implements D
 
         CapturedModel model = new CapturedModel(mp);
 
-        cactus = instancerProvider()
+        transformedInstance1 = instancerProvider()
                 .instancer(InstanceTypes.TRANSFORMED, model)
                 .createInstance();
     }
@@ -133,6 +94,8 @@ public class ArmVisual extends AbstractBlockEntityVisual<ArmEntity> implements D
     @Override
     public void collectCrumblingInstances(Consumer<@Nullable Instance> consumer) {
         instanceTree.traverse(consumer);
+        consumer.accept(transformedInstance1);
+        consumer.accept(transformedInstance2);
     }
 
     @Override
@@ -143,13 +106,15 @@ public class ArmVisual extends AbstractBlockEntityVisual<ArmEntity> implements D
             instance.light(packedLight)
                     .setChanged();
         });
-        cactus.light(packedLight);
+        transformedInstance1.light(packedLight);
+        transformedInstance2.light(packedLight);
     }
 
     @Override
     protected void _delete() {
         instanceTree.delete();
-        cactus.delete();
+        transformedInstance1.delete();
+        transformedInstance2.delete();
     }
 
     @Override
@@ -169,10 +134,14 @@ public class ArmVisual extends AbstractBlockEntityVisual<ArmEntity> implements D
             firstArm.xRot((float) meaningfulSin /2);
             secondArm.xRot((float) (meaningfulSin - Math.PI/4)/2);
 
-            cactus.setIdentityTransform();
+            transformedInstance1.setIdentityTransform();
+            transformedInstance2.setIdentityTransform();
 
-            cactus.translate(visualPos.getX(), visualPos.getY(), visualPos.getZ());
-            cactus.translate(0.5, 0, 0.5f);
+            transformedInstance1.translate(visualPos.getX(), visualPos.getY(), visualPos.getZ());
+            transformedInstance1.translate(0.5, 0, 0.5f);
+
+            transformedInstance2.translate(visualPos.getX(), visualPos.getY(), visualPos.getZ());
+            transformedInstance2.translate(0.5, 0, 0.5f);
 
             Matrix4f mx = new Matrix4f();
             baseMotor.translateAndRotate(mx);
@@ -180,11 +149,14 @@ public class ArmVisual extends AbstractBlockEntityVisual<ArmEntity> implements D
             secondArm.translateAndRotate(mx);
 
             //mx.scale(0.5f);
-            cactus.mul(mx);
+            transformedInstance1.mul(mx);
+            transformedInstance2.mul(mx);
 
-            cactus.translate(0, secondArm.initialPose().y/16f, 0);
+            transformedInstance1.translate(0, secondArm.initialPose().y/16f, 0);
+            transformedInstance2.translate(0, secondArm.initialPose().y/16f, 0);
 
-            cactus.setChanged();
+            transformedInstance1.setChanged();
+            transformedInstance2.setChanged();
 
             instanceTree.updateInstancesStatic(initialPose);
         });
