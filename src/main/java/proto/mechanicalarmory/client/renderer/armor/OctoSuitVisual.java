c8ec46c7c44c64c027c82cc6014226dc219f3e76
PrototypeTrousers <salomaoo@gmail.com>
2026-01-20 13:05:41 -0300

    ooh extra arm

--- src/main/java/proto/mechanicalarmory/client/renderer/armor/OctoSuitVisual.java
+++ src/main/java/proto/mechanicalarmory/client/renderer/armor/OctoSuitVisual.java
@@ -6,27 +6,49 @@ import dev.engine_room.flywheel.api.visualization.VisualizationContext;
 import dev.engine_room.flywheel.lib.instance.InstanceTypes;
 import dev.engine_room.flywheel.lib.instance.TransformedInstance;
 import dev.engine_room.flywheel.lib.model.Models;
+import dev.engine_room.flywheel.lib.model.part.InstanceTree;
+import dev.engine_room.flywheel.lib.model.part.ModelTree;
 import dev.engine_room.flywheel.lib.visual.SimpleDynamicVisual;
 import dev.engine_room.flywheel.lib.visual.SimpleTickableVisual;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.LevelRenderer;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.phys.Vec3;
+import org.jetbrains.annotations.Nullable;
+import org.joml.Matrix4f;
 import org.joml.Vector3f;
+import proto.mechanicalarmory.MechanicalArmoryClient;
 
 public class OctoSuitVisual implements EffectVisual<OctoSuitEffect>, SimpleDynamicVisual {
 
     TransformedInstance transformedInstance;
     Entity holderEntity;
     Vector3f visualPos;
+    ModelTree modelTree = MechanicalArmoryClient.gltfFlywheelModelTree;
+
+    private final InstanceTree instanceTree;
+    private final @Nullable InstanceTree firstArm;
+    private final @Nullable InstanceTree secondArm;
+    private final @Nullable InstanceTree baseMotor;
+    private final @Nullable InstanceTree itemAttachment;
+    Matrix4f pose = new Matrix4f();
+
 
     public OctoSuitVisual(VisualizationContext ctx, Entity holderEntity , float partialTick) {
         this.visualPos = new Vector3f((float) (holderEntity.getPosition(partialTick).x - ctx.renderOrigin().getX()),
                 (float) (holderEntity.getPosition(partialTick).y - ctx.renderOrigin().getY()),
                 (float) (holderEntity.getPosition(partialTick).z - ctx.renderOrigin().getZ()));
         this.holderEntity = holderEntity;
-        transformedInstance = ctx.instancerProvider().instancer(InstanceTypes.TRANSFORMED, Models.block(Blocks.DIRT.defaultBlockState())).createInstance();
-        transformedInstance.light(15,15);
+
+        instanceTree = InstanceTree.create(ctx.instancerProvider(), modelTree);
+        baseMotor = instanceTree.child("BaseMotor");
+        firstArm = baseMotor.child("FirstArm");
+        secondArm = firstArm.child("SecondArm");
+        itemAttachment = secondArm.child("ItemAttach");
+
+
+//        transformedInstance.light(15,15);
     }
 
     @Override
@@ -41,15 +63,35 @@ public class OctoSuitVisual implements EffectVisual<OctoSuitEffect>, SimpleDynam
 
     @Override
     public void beginFrame(Context ctx) {
-        transformedInstance.setIdentityTransform();
-        transformedInstance.translate(-0.5f, 0.5f, -0.5f);
-                transformedInstance.translate(holderEntity.getPosition(ctx.partialTick()));
-        transformedInstance.translate(
-                Vec3.directionFromRotation(
-                        0, (float) ( 180 + holderEntity.getPreciseBodyRotation(ctx.partialTick()))));
+        pose.identity();
+        pose.translate(0, 1 , 0);
 
-        transformedInstance.rotateYCentered(-holderEntity.getPreciseBodyRotation(ctx.partialTick()) * (float) (Math.PI / 180.0));
+        Vec3 ePos = holderEntity.getPosition(ctx.partialTick());
+        pose.translate((float) ePos.x(), (float) ePos.y(), (float) ePos.z());
+                Vec3 behind = Vec3.directionFromRotation(
+                0, (float) ( 180 + holderEntity.getPreciseBodyRotation(ctx.partialTick())));
+        pose.translate((float) behind.x(), (float) behind.y(), (float) behind.z());
 
-        transformedInstance.setChanged();
+        pose.rotateY(-holderEntity.getPreciseBodyRotation(ctx.partialTick()) * (float) (Math.PI / 180.0));
+
+
+
+        instanceTree.updateInstances(pose);
+
+        var packedLight = LevelRenderer.getLightColor(holderEntity.level(), holderEntity.getOnPos().above());
+        instanceTree.traverse(instance -> {
+            instance.light(packedLight)
+                    .setChanged();
+        });
+
+
+
+//        transformedInstance.setIdentityTransform();
+//        transformedInstance.translate(-0.5f, 0.5f, -0.5f);
+
+//
+//        transformedInstance.rotateYCentered(-holderEntity.getPreciseBodyRotation(ctx.partialTick()) * (float) (Math.PI / 180.0));
+//
+//        transformedInstance.setChanged();
     }
 }
